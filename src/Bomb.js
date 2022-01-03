1dd61e6e20f37c4716ef7450b385f860e02c7b11
JazzPants <leighton.l14@gmail.com>
2022-01-03 21:36:58 +1100

    Added minor changes

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,31 @@
 // your Bomb code here!
+import React from 'react';
+
+class Bomb extends React.Component {
+    constructor(props) {
+        super()
+        this.state = {
+            secondsLeft: props.initialCount
+        }
+    }
+
+    updateSeconds = () => {
+        this.setState({
+            secondsLeft: this.state.secondsLeft - 1
+        })
+    }
+
+    render() {
+        const message = this.state.secondsLeft === 0 ? 'Boom!' : this.state.secondsLeft + ' seconds left before I go boom!'
+
+        return (<div><button onClick={this.updateSeconds}>
+            {message}
+            </button>
+
+        </div>)
+    }
+
+
+}
+
+export default Bomb;
